6ff852f83d82408428cb4bc66e16ee9d79e7c72e
JuliaDivishek <divishek@rambler.ru>
2017-10-09 21:38:25 +0600

    All files

--- 2Lab4v3/main.cpp
+++ 2Lab4v3/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <stdio.h>
 
-double findMaxInt(int id, ...) {
+double sumOfRange(int id, ...) {
 	double sum;
 	int *ptr = &id;
 	//проход по всему списку параметров, до тех пор пока не встретим признак окончания последовательности 0
@@ -26,10 +26,10 @@ double findMaxInt(int id, ...) {
 }
 
 void test() {
-	printf("test1: %f == 3\n", findMaxInt(1, 3, 1, 1, 1, -1, 0) ); //3
-	printf("test2: %f == 315\n", findMaxInt(2, 5, 2, 506, 2, -196, 0, 1, 2)); //315
-	printf("test3: %f == 22.1\n", findMaxInt(3, 2.0, 3, 1.4, 3, 15.8, 3, 2.9, 0)); //22.1
-	printf("test4: %f == 156.4\n", findMaxInt(1, 5, 3, 1.4, 2, 150, 0)); //156.4
+	printf("test1: %f == 3\n", sumOfRange(1, 3, 1, 1, 1, -1, 0) ); //3
+	printf("test2: %f == 315\n", sumOfRange(2, 5, 2, 506, 2, -196, 0, 1, 2)); //315
+	printf("test3: %f == 22.1\n", sumOfRange(3, 2.0, 3, 1.4, 3, 15.8, 3, 2.9, 0)); //22.1
+	printf("test4: %f == 156.4\n", sumOfRange(1, 5, 3, 1.4, 2, 150, 0)); //156.4
 }
 
 int main() {
